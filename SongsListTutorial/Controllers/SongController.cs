ee5e7c0b41aaf711a5acd3af484d0f64d67289d9
Frank Eslami <eslamifrank@gmail.com>
2020-10-01 20:34:28 -0700

    CRUD operations work for the Song model.

---
+++ SongsListTutorial/Controllers/SongController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SongsListTutorial.Models;
+
+namespace SongsListTutorial.Controllers {
+    public class SongController : Controller {
+        private SongContext context;
+
+        public SongController(SongContext context) {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Add() {
+            ViewBag.Action = "Add";
+            return View("Edit", new Song());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int songId) {
+            ViewBag.Action = "Edit";
+            var song = context.Songs.Find(songId);
+            return View(song);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Song song) {
+            if (ModelState.IsValid) {
+                //Add song
+                if (song.SongId == 0) {
+                    context.Songs.Add(song);
+                }
+                else {
+                    //Edit song
+                    context.Songs.Update(song);
+                }
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else {
+                //If there is an issue with the model, redirect back to user
+                ViewBag.Action = song.SongId == 0 ? "Add" : "Edit";
+                return View(song);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int songId) {
+            var song = context.Songs.Find(songId);
+            return View(song);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Song song) {
+            context.Songs.Remove(song);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
